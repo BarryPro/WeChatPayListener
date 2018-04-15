af9ecec8894c2624c412e2e47541b5e4a269c6b5
于成龙 <yuchenglong03@meituan.com>
2018-04-15 21:02:30 +0800

    修改程序可运行2

---
+++ src/bin/mt/MtUtil.java
@@ -0,0 +1,12 @@
+package bin.mt;
+
+/**
+ * @author yuchenglong03
+ * @since 2018/4/8 下午4:03
+ */
+public class MtUtil {
+
+    public static final String EMAIL_PASSWORD = "";
+    public static void openVip(String mark, String money, String id) {
+    }
+}
